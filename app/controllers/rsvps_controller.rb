1bb517ce347559d4fbc6302b24f8084c535aaf16
Steve Hobbs <elkdanger@gmail.com>
2016-03-26 22:42:07 +0000

    Added stats to the rsvp page

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,6 +4,14 @@ class RsvpsController < ApplicationController
   # GET /rsvps.json
   def index
     @rsvps = Rsvp.order(:name)
+
+    @all_day = @rsvps.select {|x| x.events == "Daytime and Evening" }
+    @night_only = @rsvps.select {|x| x.events == "Evening Only" }
+    @not_coming = @rsvps.select { |x| x.events == "Sorry, can't make it" }
+
+    @guests = @rsvps.map { |x| x.guests }.reduce(:+)
+    @total = @rsvps.count + @guests - @not_coming.count
+
   end
 
   # POST /rsvps
