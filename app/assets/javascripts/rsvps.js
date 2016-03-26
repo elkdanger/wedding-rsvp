32800dbee3d98a734c773bf9d7847da1cda079e4
Steve Hobbs <elkdanger@gmail.com>
2016-03-26 23:13:29 +0000

    Added some js nicety for filtering guests

---
+++ app/assets/javascripts/rsvps.js
@@ -0,0 +1,39 @@
+'use strict'
+
+function removeHighlight() {
+  $('tr.highlight').removeClass('highlight')
+}
+
+$('#show-all-day').click(e => {
+  e.preventDefault()
+
+  removeHighlight()
+
+  $('tr').filter(function(index) {
+    return $(this).find('td:contains("Daytime and Evening")').length > 0
+  }).toggleClass('highlight')
+
+
+})
+
+$('#show-evening').click(e => {
+  e.preventDefault()
+
+  removeHighlight()
+
+  $('tr').filter(function(index) {
+    return $(this).find('td:contains("Evening Only")').length > 0
+  }).toggleClass('highlight')
+
+})
+
+$('#show-not-coming').click(e => {
+  e.preventDefault()
+
+  removeHighlight()
+
+  $('tr').filter(function(index) {
+    return $(this).find('td:contains("Sorry")').length > 0
+  }).toggleClass('highlight')
+
+})
